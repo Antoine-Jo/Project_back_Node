f59f7d4b54b5d9224aee705d2887e00b2100c919
Antoine-Jo <83338682+Antoine-Jo@users.noreply.github.com>
2021-11-12 16:58:46 +0100

    Ajout update/delete User

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -15,3 +15,39 @@ module.exports.userInfo = (req, res) => {
         else console.log('ID unknown : ' + err);
     }).select('-password');
 };
+
+module.exports.updateUser = async (req, res) => {
+    if(!ObjectId.isValid(req.params.id))
+        return res.status(400).send('ID unknown : ' + req.params.id)
+
+    try {
+        await UserModel.findOneAndUpdate(
+            {_id: req.params.id},
+            {
+                $set: {
+                    bio: req.body.bio
+                }
+            },
+            {new: true, upsert: true, setDefaultsOnInsert: true},
+            (error, docs) => {
+                if(!error) return res.send(docs);
+                if(error) return res.status(500).send({ message: error });
+            }
+        )
+    } 
+    catch (error) {
+        return res.status(500).json({ message: error });
+    }
+};
+
+module.exports.deleteUser = async (req, res) => {
+    if(!ObjectId.isValid(req.params.id))
+        return res.status(400).send('ID unknown : ' + req.params.id)
+
+    try {
+        await UserModel.deleteOne({ _id: req.params.id}).exec();
+        res.status(200).json({ message: "Succesfully deleted. " });
+    } catch (error) {
+        return res.status(500).json({ message: error });
+    }
+}
