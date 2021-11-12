186d5aeae84cdeb21a61ece9894ae329a6b2083f
Antoine-Jo <83338682+Antoine-Jo@users.noreply.github.com>
2021-11-12 18:05:58 +0100

    Ajout follower

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -51,3 +51,43 @@ module.exports.deleteUser = async (req, res) => {
         return res.status(500).json({ message: error });
     }
 }
+
+module.exports.follow = async (req, res) => {
+    if(!ObjectId.isValid(req.params.id) || !ObjectId.isValid(req.body.idToFollow))
+        return res.status(400).send('ID unknown : ' + req.params.id)
+
+    try {
+        // add to the follower list
+        await UserModel.findByIdAndUpdate(
+            req.params.id,
+            { $addToSet: { following: req.body.idToFollow }},
+            { new: true, upsert: true },
+            (error, docs) => {
+                if(!error) res.status(201).json(docs);
+                else return res.status(400).json(error);
+            }
+        );
+        // add to following list
+        await UserModel.findByIdAndUpdate(
+            req.body.idToFollow,
+            { $addToSet: { followers: req.params.id }},
+            { new: true, upsert: true},
+            (error, docs) => {
+                if(error) return res.status(400).json(error);
+            }
+        );
+    } catch (error) {
+        return res.status(500).json({ message: error });
+    }
+}
+
+module.exports.unfollow = async (req, res) => {
+    if(!ObjectId.isValid(req.params.id))
+        return res.status(400).send('ID unknown : ' + req.params.id)
+
+    try {
+
+    } catch (error) {
+        return res.status(500).json({ message: error });
+    }
+}
